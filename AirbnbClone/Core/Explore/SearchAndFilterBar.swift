3aee8021f13ca8aa2a47556229575de879660dcf
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-05-10 15:36:09 +0200

    search bar

---
+++ AirbnbClone/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAndFilterBar.swift
+//  AirbnbClone
+//
+//  Created by Lorenzo Menino on 10/05/2024.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchAndFilterBar()
+}
