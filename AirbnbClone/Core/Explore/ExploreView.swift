93e725a8606dd8565617854e1a5926ce70ec2bf3
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-05-10 15:36:25 +0200

    search bar

--- AirbnbClone/Core/Explore/ExploreView.swift
+++ AirbnbClone/Core/Explore/ExploreView.swift
@@ -10,14 +10,22 @@ import SwiftUI
 struct ExploreView: View {
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVStack(spacing: 32) {
-                    // Charge le contenu uniquement au défilement
-                    ForEach(0...10, id : \.self) { listing in
-                        ListingItemView()
-                            .frame(height: 400)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
+            VStack {
+                SearchAndFilterBar()
+                ScrollView {
+                    LazyVStack(spacing: 32) {
+                        // Charge le contenu uniquement au défilement
+                        ForEach(0...10, id : \.self) { listing in
+                            NavigationLink(value: listing) {
+                                ListingItemView()
+                                    .frame(height: 400)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            }
+                        }
                     }
+                }
+                .navigationDestination(for: Int.self) { listing in
+                    Text("Listing Detail view")
                 }
             }
         }
