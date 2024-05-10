231baf6b84a8a6e1e7b269168886f617cafe936b
LorenzoM27 <94546130+LorenzoM27@users.noreply.github.com>
2024-05-10 09:53:34 +0100

    Add files via upload

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Lorenzo Menino on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
