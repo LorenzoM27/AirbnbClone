a5d5a25015c0d29415f617b3f27b46def28df8e6
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-05-10 13:57:15 +0200

    listing items

---
+++ AirbnbClone/Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  AirbnbClone
+//
+//  Created by Lorenzo Menino on 10/05/2024.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
